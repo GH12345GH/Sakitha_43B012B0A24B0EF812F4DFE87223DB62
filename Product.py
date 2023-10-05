c3b49c47903bd05319616ff314460040d73063e3
GH12345GH <144374343+GH12345GH@users.noreply.github.com>
2023-10-06 00:31:07 +0530

    Create Product.py

---
+++ Product.py
@@ -0,0 +1,10 @@
+def linearsearchproduct(productlist,targetproduct);
+indices=[] for index, product in enumerate(product);
+if product==targetproduct;
+indices.append(index)
+return indices
+products=["shoes","boot","loafer","shoes","sandal","shoes","sandal","shoes","sandal"];
+target="shoes"
+target2="mobile"
+result=linearsearchproduct(product, target)
+print (result)
